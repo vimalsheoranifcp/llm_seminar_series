406d25e321ea7c46d10d6bc1c1ecb11770dee254
vimalsheoranifcp <vimal@infocusp.com>
2024-03-11 19:52:18 +0530

    Added utilities for calling model with batch inputs and parsing output.

---
+++ session_2/utils.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import time
+
+def predict_batch(model, data, prompt, batch_size=30):
+    ratings = []
+    for i in range(0, data.shape[0] - batch_size + 1):
+        review_id = data.iloc[i:i+batch_size]['review_id'].tolist()
+        review_text = data.iloc[i:i+batch_size]['review_text'].tolist()
+        user_input = "\n".join([f"{_id}|{txt}" for _id, txt in zip(review_id, review_text)])
+        prediction = model.invoke(prompt.format(user_input))
+        ratings.append(prediction.content)
+        time.sleep(1)
+    return ratings
+
+def parse_model_predicted_ratings(raw_predictions):
+    predicted_ratings = []
+    for r in raw_predictions:
+        individual_ratings = r.split("\n")
+        for i in individual_ratings:
+            try:
+                predicted_ratings.append(
+                    {
+                        'review_id': int(i.split("|")[0]),
+                        'rating': int(i.split("|")[1])
+                    }
+                )
+            except ValueError as e:
+                continue
+    return pd.DataFrame(predicted_ratings)
