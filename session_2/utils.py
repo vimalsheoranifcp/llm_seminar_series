efb839fcff72b62c94de8ccf17404bb7d05d7eb9
vinaykakkad <56934712+vinaykakkad@users.noreply.github.com>
2024-04-09 09:40:00 +0530

    Adds utils for cot/sc chain and generates ratings

--- session_2/utils.py
+++ session_2/utils.py
@@ -21,12 +21,50 @@ def parse_model_predicted_ratings(raw_predictions):
         individual_ratings = r.split("\n")
         for i in individual_ratings:
             try:
-                predicted_ratings.append(
-                    {
-                        'review_id': int(i.split("|")[0]),
-                        'rating': int(i.split("|")[1])
-                    }
-                )
+                review_id = i.split("|")[0]
+                rating = i.split("|")[1]
+                if not (0 < rating < 10):
+                    raise ValueError(f"Invalid rating: {rating} for rating string '{i}' for raw prediction {r}")
+                predicted_ratings.append({'review_id': review_id, 'rating': rating})
             except ValueError as e:
+                print(f"Error while parsing predicted rating: {e}")
                 continue
     return pd.DataFrame(predicted_ratings)
+
+def parse_cot_output(output, start_tag="<rating>", end_tag="</rating>"):
+    try:
+        start = output.find(start_tag)
+        if start == -1:
+            raise ValueError("Couldn't find start tag.")
+        start_tag_length = len(start_tag)
+        end = output.find(end_tag, start + start_tag_length)
+        if end == -1:
+            raise ValueError("Couldn't find end tag.")
+        return int(output[start+start_tag_length:end])
+    except Exception as e:
+        print(f"Error while parsing output: {e}\nOutput:{output}")
+        return -1
+
+def cot_parsing_chain(review, prompt, llm, output_parser):
+    try:
+        output = llm.invoke(prompt.format(review))
+        final_rating = output_parser(output.content)
+        return final_rating
+    except Exception as e:
+        print(f"Error while generating prediction: {e}")
+        return -1
+    
+def get_combined_rating(ratings):
+    non_erroneous_ratings = [rating for rating in ratings if rating != -1]
+    if len(non_erroneous_ratings) == 0:
+        raise ValueError("All the ratings are erroneous")
+    return round(sum(non_erroneous_ratings) / len(non_erroneous_ratings))
+
+async def sc_parsing_chain(review, prompt, llm, output_parser, samples=5):
+    try:
+        outputs = await llm.abatch([prompt.format(review) for _ in range(samples)])
+        parsed_outputs = [output_parser(output.content) for output in outputs]
+        return get_combined_rating(parsed_outputs), parsed_outputs
+    except Exception as e:
+        print(f"Error while generating prediction: {e}")
+        return -1, []
